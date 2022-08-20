4b535d08d76aed69f78714ce42113d129cf99274
Egoist <epsilonelittr@gmail.com>
2022-08-21 01:14:30 +0300

    Update readme

--- README.md
+++ README.md
@@ -2,6 +2,33 @@
 
 My awesome tailwind templates.
 
-# ⚠️ Caution
+## Content List
 
-This repository only for demonstration. If you want to use tailwind please use with **[purge](https://tailwindcss.com/docs/optimizing-for-production#writing-purgeable-html)** option. Default tailwind.min.css comes with about ~2MB and this is not best practise. You can reduce template **[sizes](https://tailwindcss.com/docs/optimizing-for-production)**.
+<!-- TABLE_START -->
+
+- [clones](src\clones)
+	- [google-translate.html](src\clones\google-translate.html)
+- [components](src\components)
+	- [alert-bars.html](src\components\alert-bars.html)
+	- [label-badges.html](src\components\label-badges.html)
+	- [menu-nav.html](src\components\menu-nav.html)
+	- [mobile-navigation-bar.html](src\components\mobile-navigation-bar.html)
+	- [navbar.html](src\components\navbar.html)
+	- [panzoom-control.html](src\components\panzoom-control.html)
+	- [player-slider.html](src\components\player-slider.html)
+	- [question-listing-item-card.html](src\components\question-listing-item-card.html)
+	- [scroll-me-button.html](src\components\scroll-me-button.html)
+	- [spinners.html](src\components\spinners.html)
+	- [story-bar.html](src\components\story-bar.html)
+	- [themeforest-product-card.html](src\components\themeforest-product-card.html)
+	- [user-profile-card.html](src\components\user-profile-card.html)
+- [concept](src\concept)
+	- [spotify](src\concept\spotify)
+		- [player.html](src\concept\spotify\player.html)
+- [templates](src\templates)
+	- [404-page-template.html](src\templates\404-page-template.html)
+	- [login-form.html](src\templates\login-form.html)
+	- [our-team.html](src\templates\our-team.html)
+	- [responsive-footer.html](src\templates\responsive-footer.html)
+	- [social-media-landing-page-mobile.html](src\templates\social-media-landing-page-mobile.html)
+<!-- TABLE_END -->
---
+++ src/markdown.js
@@ -0,0 +1,73 @@
+const fs = require("fs")
+const path = require("path")
+
+/**
+ * Get array of file/folder list from a target directory
+ * 
+ * @param dirPath 
+ * @param fileArray 
+ * @returns Array
+ * 
+ * @source https://coderrocketfuel.com/article/recursively-list-all-the-files-in-a-directory-using-node-js
+ */
+function getAllFiles(dirPath, fileArray, depth = 0) {
+    files = fs.readdirSync(dirPath);
+
+    fileArray = fileArray || [];
+
+    files.forEach(function (file) {
+        // ignore this file
+        if (file !== 'markdown.js') {
+            // get each path
+            fileArray.push({
+                name: file,
+                path: path.join('./src/', dirPath, file),
+                depth: depth
+            });
+            // fileArray.push('\t'.repeat(depth) + '- ' +  path.join('./src/', dirPath, file));
+    
+            // also get sub-folders and contents
+            if (fs.statSync(dirPath + "/" + file).isDirectory()) {
+                fileArray = getAllFiles(dirPath + "/" + file, fileArray, depth + 1);
+            }
+        }
+    })
+
+    return fileArray;
+}
+
+/**
+ * Update readme content tree
+ * 
+ * @param newData 
+ */
+function updateList(newData) {
+    fs.readFile('./../README.md', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+
+            return;
+        }
+
+        if (data) {
+            const match = data.match('<!-- TABLE_START -->((.|\n|\s|\r)*)<!-- TABLE_END -->');
+
+            if (match && match[1]) {
+                data = data.replace(match[1], `\n\n${newData}\n`);
+
+                new Promise(function (resolve, reject) {
+                    fs.writeFile('./../README.md', data, 'utf8', function (err) {
+                        if (err) reject(err);
+                        else resolve(data);
+                    });
+                });
+            }
+        }
+    });
+}
+
+const folderTree = getAllFiles('./').map((x) => `${'\t'.repeat(x.depth)}- [${x.name}](${x.path})`).join('\n');
+
+updateList(folderTree);
+
+console.log(folderTree);
